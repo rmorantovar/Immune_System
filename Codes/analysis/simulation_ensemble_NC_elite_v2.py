2473f466dd1c7fd5d2e47f87e51db0343a21c970
Roberto Moran-Tovar <r.morantovar@uni-koeln.de>
2022-10-18 18:27:34 +0200

    18.10.22 iMac

--- Codes/analysis/simulation_ensemble_NC_elite_v2.py
+++ Codes/analysis/simulation_ensemble_NC_elite_v2.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore")
 Text_files_path = '/Users/robertomorantovar/Dropbox/Research/Evolution_Immune_System/Text_files/'
 
 #--------------- PARAMETERS ---------------------
-N_ensss = [[200], [400, 100]]
+N_ensss = [[200], [400, 300, 100]]
 N_r = 2e8
 
 T0 = 3
