0e8a3c4742dc62a79e0906f592fb1a81fe0b7d3a
Roberto <r.morantovar@uni-koeln.de>
2025-08-07 12:17:33 +0200

    Update Z_N.py

--- Codes/analysis/memory_response/Z_N.py
+++ Codes/analysis/memory_response/Z_N.py
@@ -1,10 +1,9 @@
 import sys
-sys.path.append('../../lib/')
+sys.path.append('../../my_lib/')
 from funcs import*
 from matplotlib.colors import LogNorm
 from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
-from scipy.signal import medfilt
 
 def main():
 	parser = argparse.ArgumentParser(description="Generate random sequences and save properties to a CSV file.")
@@ -70,160 +69,161 @@ def main():
 	project = 'memory_response'
 	subproject = 'multi-epitope'
 	subproject = 'Z_dynamics'
-	experiment = args.exp
-	root_dir = f"/Users/robertomorantovar/Dropbox/Research/Immune_system/{project}/{subproject}/{experiment}"
-	pars_dir_1 = f"/L0-{int(L0/10**int(np.log10(L0)))}e{int(np.log10(L0))}_p-{p}_k_step-{int(k_step)}_lamA-{lamA}_lamB-{lamB}"
-	pars_dir_2 = f"/N_ant-{N_ant}_N_ens-{N_ens}_N_epi-{N_epi}_N_evo-{N_evo}"
-	antigens_data = pd.read_csv(root_dir + pars_dir_1 + pars_dir_2 + "/antigens.csv", converters={"antigen": literal_eval})
-	antigens = antigens_data['antigen']
-	output_plot = '../../../Figures/'+project+'/'+subproject+'/'+str(experiment)
-	os.makedirs(output_plot, exist_ok=True)
+	for exp in [1, 7]:
+		experiment = exp
+		root_dir = f"/Users/robertomorantovar/Dropbox/Research/Immune_system/{project}/{subproject}/{experiment}"
+		pars_dir_1 = f"/L0-{int(L0/10**int(np.log10(L0)))}e{int(np.log10(L0))}_p-{p}_k_step-{int(k_step)}_lamA-{lamA}_lamB-{lamB}"
+		pars_dir_2 = f"/N_ant-{N_ant}_N_ens-{N_ens}_N_epi-{N_epi}_N_evo-{N_evo}"
+		antigens_data = pd.read_csv(root_dir + pars_dir_1 + pars_dir_2 + "/antigens.csv", converters={"antigen": literal_eval})
+		antigens = antigens_data['antigen']
+		output_plot = '/Users/robertomorantovar/Dropbox/My_Documents/Science/Projects/Immune_System/_Repository/Figures/'+project+'/'+subproject+'/'+str(experiment)
+		os.makedirs(output_plot, exist_ok=True)
 
-	N_ant = len(antigens[:])
+		N_ant = len(antigens[:])
 
-	for a1, antigen1 in enumerate(antigens[:]):
-		if a1==0:
-			figZ, axZ = plt.subplots(figsize=(5*1.62, 5), gridspec_kw={'left':0.10, 'right':.95, 'bottom':.1, 'top': 0.95})
-			figN_A, axN_A = plt.subplots(figsize=(5*1.62, 5), gridspec_kw={'left':0.10, 'right':.95, 'bottom':.1, 'top': 0.95})
-			# antigen_seq = from_aa_to_i(antigen1, energy_model, '../../')
-			#--------------------------Energy Motif--------------------------
-			motif = get_motif(antigen1, energy_model, '../../')*1.2
+		for a1, antigen1 in enumerate(antigens[:]):
+			if a1==0:
+				figZ, axZ = plt.subplots(figsize=(8*1.62,8), gridspec_kw={'left':0.12, 'right':.95, 'bottom':.15, 'top': 0.94})
+				figN_A, axN_A = plt.subplots(figsize=(8*1.62,8), gridspec_kw={'left':0.12, 'right':.95, 'bottom':.15, 'top': 0.94})
+				# antigen_seq = from_aa_to_i(antigen1, energy_model, '../../')
+				#--------------------------Energy Motif--------------------------
+				motif = get_motif(antigen1, energy_model, '../../')*1.2
 
-			#Change values by the minimum
-			E_m = -3
-			for i in np.arange(l):
-				E_m+=np.min(motif[:,i], axis=0)
-				motif[:,i]-=np.min(motif[:,i], axis=0)
-			# print('Em:', E_m)
-			#--------------------------Entropy function--------------------------
-			Es, dE, Q0, betas = calculate_Q0(0.01, 50, 400000, motif, E_m, l)
-			Kds = np.exp(Es[:-1])
-			#--------------------------Repertoire properties--------------------------
-			beta_r, E_r, Kd_r = get_repertoire_properties(betas, Q0, Es, dE, L0)
+				#Change values by the minimum
+				E_m = -3
+				for i in np.arange(l):
+					E_m+=np.min(motif[:,i], axis=0)
+					motif[:,i]-=np.min(motif[:,i], axis=0)
+				# print('Em:', E_m)
+				#--------------------------Entropy function--------------------------
+				Es, dE, Q0, betas = calculate_Q0(0.01, 50, 400000, motif, E_m, l)
+				Kds = np.exp(Es[:-1])
+				#--------------------------Repertoire properties--------------------------
+				beta_r, E_r, Kd_r = get_repertoire_properties(betas, Q0, Es, dE, L0)
 
-			dataNaive = pd.read_csv(root_dir + pars_dir_1 + pars_dir_2 + '/%d'%(a1+1) + '/activated_repertoire.csv')#, converters={"N_t": literal_eval})
-			dataMemory = pd.read_csv(root_dir + pars_dir_1 + pars_dir_2 + '/%d'%(a1+1) + '/%d'%(a1) + '/activated_repertoire.csv')#, converters={"N_t": literal_eval})
-			E_ms = []
+				dataNaive = pd.read_csv(root_dir + pars_dir_1 + pars_dir_2 + '/%d'%(a1+1) + '/activated_repertoire.csv')#, converters={"N_t": literal_eval})
+				dataMemory = pd.read_csv(root_dir + pars_dir_1 + pars_dir_2 + '/%d'%(a1+1) + '/%d'%(a1) + '/activated_repertoire.csv')#, converters={"N_t": literal_eval})
+				E_ms = []
 
-			ZNaive = np.zeros_like(time_array)
-			NNaive = np.zeros_like(time_array)
-			ZMemory = np.zeros_like(time_array)
-			NMemory = np.zeros_like(time_array)
+				ZNaive = np.zeros_like(time_array)
+				NNaive = np.zeros_like(time_array)
+				ZMemory = np.zeros_like(time_array)
+				NMemory = np.zeros_like(time_array)
 
-			# N_ens = 2
+				# N_ens = 2
 
-			for i in tqdm(range(N_ens)):
-				#----------NAIVE----------
-				dataNaive_i = dataNaive[dataNaive['ens_id']==i].reset_index()
-				E_ms.append(np.min(dataNaive_i['E']))
-				dataNaive_i_t = ensemble_of_expansions_time(dataNaive_i, N_ens, p, time_array, lamB, C, dT)
-				ZNaive_i = dataNaive_i_t.apply(lambda row: np.array(row['N_t']) / np.exp(row['E']), axis=1).sum()
-				NNaive_i = dataNaive_i_t.apply(lambda row: np.array(row['N_t']), axis=1).sum()
-				with np.errstate(divide='ignore', invalid='ignore'):
-					pbNaive = (1+(1/(1e-9 *ZNaive_i)))**(-1)
+				for i in tqdm(range(N_ens)):
+					#----------NAIVE----------
+					dataNaive_i = dataNaive[dataNaive['ens_id']==i].reset_index()
+					E_ms.append(np.min(dataNaive_i['E']))
+					dataNaive_i_t = ensemble_of_expansions_time(dataNaive_i, N_ens, p, time_array, lamB, C, dT)
+					ZNaive_i = dataNaive_i_t.apply(lambda row: np.array(row['N_t']) / np.exp(row['E']), axis=1).sum()
+					NNaive_i = dataNaive_i_t.apply(lambda row: np.array(row['N_t']), axis=1).sum()
+					with np.errstate(divide='ignore', invalid='ignore'):
+						pbNaive = (1+(1/(1e-9 *ZNaive_i)))**(-1)
 
-				# Interpolate f(t) for use in ODE
-				pbNaive_interp = interp1d(time_array, pbNaive, kind='linear', fill_value='extrapolate')
+					# Interpolate f(t) for use in ODE
+					pbNaive_interp = interp1d(time_array, pbNaive, kind='linear', fill_value='extrapolate')
 
-				# Define the ODE: dN/dt = lambda * (1 - f(t)) * N
-				def dNdtNaive(t, N):
-				    return (lamA * (1 - pbNaive_interp(t)) - 1.) * N
-
-				# Initial condition
-				N0 = 1.0
-
-				# Solve the ODE over the time span of your data
-				solNaive = solve_ivp(dNdtNaive, t_span=(time_array[0], time_array[-1]), y0=[N0], t_eval=time_array)
-
-				# Result
-				N_ANaive_i = solNaive.y[0]  # solution N(t) evaluated at t_vals
-
-				if i%20==0:
-					axZ.plot(NNaive_i.astype(float), ZNaive_i, color = my_blue, lw = .2, alpha = .2, ls = '-', marker = '.', ms = 0.5)
-					axN_A.plot(time_array, N_ANaive_i, color = my_blue, lw = 2, alpha = .5, ls = '-')
-					if i == 0:
-						axN_A.plot(time_array, N_ANaive_i, color = my_blue, lw = 2, alpha = .5, ls = '-', label = r'$\textrm{Naive}$')
-
-				#----------MEMORY----------
-				dataMemory_i = dataMemory[dataMemory['ens_id']==i].reset_index()
-				# E_ms.append(np.min(dataMemory_i['E']))
-				dataMemory_i_t = ensemble_of_expansions_time(dataMemory_i, N_ens, p, time_array, lamB, C, dT)
-				ZMemory_i = dataMemory_i_t.apply(lambda row: np.array(row['N_t']) / np.exp(row['E']), axis=1).sum()
-				NMemory_i = dataMemory_i_t.apply(lambda row: np.array(row['N_t']), axis=1).sum()
-				with np.errstate(divide='ignore', invalid='ignore'):
-					pbMemory = (1+(1/(1e-9 *ZMemory_i)))**(-1)
-
-				# Interpolate f(t) for use in ODE
-				pbMemory_interp = interp1d(time_array, pbMemory, kind='linear', fill_value='extrapolate')
-
-				# Define the ODE: dN/dt = lambda * (1 - f(t)) * N
-				def dNdtMemory(t, N):
-				    return (lamA * (1 - pbMemory_interp(t)) - 1.) * N
-
-				# Initial condition
-				N0 = 1.0
-
-				# Solve the ODE over the time span of your data
-				solMemory = solve_ivp(dNdtMemory, t_span=(time_array[0], time_array[-1]), y0=[N0], t_eval=time_array)
-
-				# Result
-				N_AMemory_i = solMemory.y[0]  # solution N(t) evaluated at t_vals
-
-				if i%20==0:
-					axZ.plot(NMemory_i.astype(float), ZMemory_i, color = my_purple, lw = .2, alpha = .2, ls = '-', marker = '.', ms = 0.5)
-					axN_A.plot(time_array, N_AMemory_i, color = my_purple, lw = 2, alpha = .5, ls = '-')
-					if i == 0:
-						axN_A.plot(time_array, N_AMemory_i, color = my_purple, lw = 2, alpha = .5, ls = '-', label = r'$\textrm{Memory}$')
-								
-
-				with np.errstate(divide='ignore', invalid='ignore'):
-					NNaive+=np.log(NNaive_i)
-					NMemory+=np.log(NMemory_i)
-					ZNaive+=np.log(ZNaive_i)
-					ZMemory+=np.log(ZMemory_i)
-
-			
-			ZNaive = np.exp(ZNaive/N_ens)
-			ZMemory = np.exp(ZMemory/N_ens)
-			NNaive = np.exp(NNaive/N_ens)
-			NMemory = np.exp(NMemory/N_ens)
-
-			axZ.plot(NNaive, ZNaive, color = my_blue, lw = 3, alpha = .9, ls = '-', label = r'$\textrm{Naive}$')
-			axZ.plot(NMemory, ZMemory, color = my_purple, lw = 3, alpha = .9, ls = '-', label = r'$\textrm{Memory}$')
-
-			lamZ = np.max([lamB, lamA/p*(beta_r-1)])
-			print(lamZ)
-
-			Z_mf = Kstep**(1/v)/(Kd_r)**(1+1/v)*np.exp(lamZ*(time_array - t0))
-			# axZ.plot(time_array[(time_array<8) & (time_array>t0)], Z_mf[(time_array<8) & (time_array>t0)], alpha = .5, color = my_blue, lw = 2, ls = '--')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
-			Z_mf = Kstep**(1/v)/(Kd_r)**(1+1/v)*np.exp(lamB*(time_array - t0))
-			# axZ.plot(time_array[(time_array<8) & (time_array>t0)], Z_mf[(time_array<8) & (time_array>t0)], alpha = .5, color = my_blue, lw = 2, ls = ':')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
-
-			axZ.plot(np.logspace(0, 4.1, 100), (1/(Kd_r))*np.logspace(0, 4.1, 100), alpha = 1, color = 'k', lw = 2, ls = ':')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
-			
-			if experiment == 7:
-				Z_mf = 0.1*C*Kstep**(1/v)/(Kd_r)**(1+1/v)*np.exp(lamB*(time_array - t0))
-				# axZ.plot(time_array[(time_array<6) & (time_array>t0)], Z_mf[(time_array<6) & (time_array>t0)], alpha = .5, color = my_purple, lw = 2, ls = '--')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
-			
-			# axZ.hlines(1/Kd_r, 0, T, color = 'k', ls = '--')
-
-			my_plot_layout(ax = axZ, xscale='log', yscale= 'log', ticks_labelsize= 24, x_fontsize=30, y_fontsize=30)
-			# axZ.set_xticks([])
-			# axZ.set_yticks([])
-			axZ.set_ylim(bottom = 2e5, top = 1e11)
-			# axZ.set_xlim(left = 1, right = 8)
-			axZ.legend(fontsize = 18, loc = 2)
-			figZ.savefig(output_plot + '/Z_%d_t.pdf'%(a1+1))
-
-			my_plot_layout(ax = axN_A, xscale='linear', yscale= 'log', ticks_labelsize= 24, x_fontsize=30, y_fontsize=30)
-			# axN_A.set_xticks([])
-			# axN_A.set_yticks([])
-			axN_A.set_ylim(bottom = 1e4, top = 2e13)
-			axN_A.set_xlim(left = 1, right = 8)
-			axN_A.legend(fontsize = 18, loc = 2)
-			figN_A.savefig(output_plot + '/N_A_%d_t.pdf'%(a1+1))
+					# Define the ODE: dN/dt = lambda * (1 - f(t)) * N
+					def dNdtNaive(t, N):
+						return (lamA * (1 - pbNaive_interp(t)) - 1.5) * N
 					
+					# Initial condition
+					N0 = 1.0
+					
+					# Solve the ODE over the time span of your data
+					solNaive = solve_ivp(dNdtNaive, t_span=(time_array[0], time_array[-1]), y0=[N0], t_eval=time_array)
+
+					# Result
+					N_ANaive_i = solNaive.y[0]  # solution N(t) evaluated at t_vals
+
+					if i%20==0:
+						axZ.plot(NNaive_i.astype(float), ZNaive_i, color = my_blue, lw = .2, alpha = .2, ls = '-', marker = '.', ms = 0.5)
+						axN_A.plot(time_array, N_ANaive_i, color = my_blue, lw = 2, alpha = .5, ls = '-')
+						if i == 0:
+							axN_A.plot(time_array, N_ANaive_i, color = my_blue, lw = 2, alpha = .5, ls = '-', label = r'$\textrm{Naive}$')
+
+					#----------MEMORY----------
+					dataMemory_i = dataMemory[dataMemory['ens_id']==i].reset_index()
+					# E_ms.append(np.min(dataMemory_i['E']))
+					dataMemory_i_t = ensemble_of_expansions_time(dataMemory_i, N_ens, p, time_array, lamB, C, dT)
+					ZMemory_i = dataMemory_i_t.apply(lambda row: np.array(row['N_t']) / np.exp(row['E']), axis=1).sum()
+					NMemory_i = dataMemory_i_t.apply(lambda row: np.array(row['N_t']), axis=1).sum()
+					with np.errstate(divide='ignore', invalid='ignore'):
+						pbMemory = (1+(1/(1e-9 *ZMemory_i)))**(-1)
+
+					# Interpolate f(t) for use in ODE
+					pbMemory_interp = interp1d(time_array, pbMemory, kind='linear', fill_value='extrapolate')
+
+					# Define the ODE: dN/dt = lambda * (1 - f(t)) * N
+					def dNdtMemory(t, N):
+						return (lamA * (1 - pbMemory_interp(t)) - 1.2) * N
+
+					# Initial condition
+					N0 = 1.0
+
+					# Solve the ODE over the time span of your data
+					solMemory = solve_ivp(dNdtMemory, t_span=(time_array[0], time_array[-1]), y0=[N0], t_eval=time_array)
+
+					# Result
+					N_AMemory_i = solMemory.y[0]  # solution N(t) evaluated at t_vals
+
+					if i%20==0:
+						axZ.plot(NMemory_i.astype(float), ZMemory_i, color = my_purple, lw = .2, alpha = .2, ls = '-', marker = '.', ms = 0.5)
+						axN_A.plot(time_array, N_AMemory_i, color = my_purple, lw = 2, alpha = .5, ls = '-')
+						if i == 0:
+							axN_A.plot(time_array, N_AMemory_i, color = my_purple, lw = 2, alpha = .5, ls = '-', label = r'$\textrm{Memory}$')
+									
+
+					with np.errstate(divide='ignore', invalid='ignore'):
+						NNaive+=np.log(NNaive_i)
+						NMemory+=np.log(NMemory_i)
+						ZNaive+=np.log(ZNaive_i)
+						ZMemory+=np.log(ZMemory_i)
+
+				
+				ZNaive = np.exp(ZNaive/N_ens)
+				ZMemory = np.exp(ZMemory/N_ens)
+				NNaive = np.exp(NNaive/N_ens)
+				NMemory = np.exp(NMemory/N_ens)
+
+				axZ.plot(NNaive, ZNaive, color = my_blue, lw = 3, alpha = .9, ls = '-', label = r'$\textrm{Naive}$')
+				axZ.plot(NMemory, ZMemory, color = my_purple, lw = 3, alpha = .9, ls = '-', label = r'$\textrm{Memory}$')
+
+				lamZ = np.max([lamB, lamA/p*(beta_r-1)])
+				print(lamZ)
+
+				Z_mf = Kstep**(1/v)/(Kd_r)**(1+1/v)*np.exp(lamZ*(time_array - t0))
+				# axZ.plot(time_array[(time_array<8) & (time_array>t0)], Z_mf[(time_array<8) & (time_array>t0)], alpha = .5, color = my_blue, lw = 2, ls = '--')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
+				Z_mf = Kstep**(1/v)/(Kd_r)**(1+1/v)*np.exp(lamB*(time_array - t0))
+				# axZ.plot(time_array[(time_array<8) & (time_array>t0)], Z_mf[(time_array<8) & (time_array>t0)], alpha = .5, color = my_blue, lw = 2, ls = ':')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
+
+				axZ.plot(np.logspace(0, 4.1, 100), (1/(Kd_r))*np.logspace(0, 4.1, 100), alpha = 1, color = 'k', lw = 2, ls = ':')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
+				
+				if experiment == 7:
+					Z_mf = 0.1*C*Kstep**(1/v)/(Kd_r)**(1+1/v)*np.exp(lamB*(time_array - t0))
+					# axZ.plot(time_array[(time_array<6) & (time_array>t0)], Z_mf[(time_array<6) & (time_array>t0)], alpha = .5, color = my_purple, lw = 2, ls = '--')#, label = r'$\textrm{Mean-field}\, \textrm{approx}$')
+				
+				# axZ.hlines(1/Kd_r, 0, T, color = 'k', ls = '--')
+
+				my_plot_layout(ax = axZ, xscale='log', yscale= 'log', ticks_labelsize= 40, x_fontsize=30, y_fontsize=30)
+				# axZ.set_xticks([])
+				# axZ.set_yticks([])
+				axZ.set_ylim(bottom = 2e5, top = 1e11)
+				# axZ.set_xlim(left = 1, right = 8)
+				axZ.legend(fontsize = 30, loc = 2)
+				figZ.savefig(output_plot + '/Z_%d_t.pdf'%(a1+1))
+
+				my_plot_layout(ax = axN_A, xscale='linear', yscale= 'log', ticks_labelsize= 40, x_fontsize=30, y_fontsize=30)
+				# axN_A.set_xticks([])
+				# axN_A.set_yticks([])
+				axN_A.set_ylim(bottom = 1e4, top = 2e13)
+				axN_A.set_xlim(left = 1, right = 8)
+				axN_A.legend(fontsize = 30, loc = 2)
+				figN_A.savefig(output_plot + '/N_A_%d_t.pdf'%(a1+1))
+						
 
 if __name__ == "__main__":
-    main()
+	main()
 
